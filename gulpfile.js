fa334ebc2a2ab3dd856db6d090d74f29d4a8f91d
Henrik Karlsson <hk.henrik@gmail.com>
2015-03-17 23:03:58 +0100

    Not crashing gulp when there's an error while watching

--- gulpfile.js
+++ gulpfile.js
@@ -4,10 +4,24 @@ var gulp = require('gulp'),
 	stylus = require('gulp-stylus'),
 	jade = require('gulp-jade'),
 	sourcemaps = require('gulp-sourcemaps'),
-	livereload = require('gulp-livereload');
+	livereload = require('gulp-livereload'),
+	plumber = require('gulp-plumber'),
+	gutil = require('gulp-util');
+
+var watching = false;
+
+function exitIfNotWatching(error) {
+	if (watching) {
+		gutil.log(error.message);
+	}
+	else {
+		throw error;
+	}
+}
 
 gulp.task('styles', function() {
 	gulp.src('./app/styl/app.styl')
+		.pipe(plumber(exitIfNotWatching))
 		.pipe(sourcemaps.init())
 		.pipe(stylus({
 			compress: true
@@ -19,12 +33,14 @@ gulp.task('styles', function() {
 
 gulp.task('templates', function() {
 	gulp.src('./app/templates/*.jade')
+		.pipe(plumber(exitIfNotWatching))
 		.pipe(jade())
 		.pipe(gulp.dest('./out/'))
 		.pipe(livereload());
 });
 
 gulp.task('watch', function() {
+	watching = true;
 	gulp.start('default');
 	livereload.listen();
 	gulp.watch('./app/styl/*', ['styles']);
