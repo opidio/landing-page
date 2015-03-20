564342ec2ac2677f3a3bc54e311e8d2c3cdfc258
Henrik Karlsson <hk.henrik@gmail.com>
2015-03-20 01:28:53 +0100

    Added browserify + babel scripts
    The requires in the gulpfile has really turned into a mess and needs a cleanup of unused ones.
    
    app.js and sub.js are just placeholders

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,20 @@
 'use strict';
 
 var gulp = require('gulp'),
-	stylus = require('gulp-stylus'),
-	jade = require('gulp-jade'),
-	sourcemaps = require('gulp-sourcemaps'),
-	livereload = require('gulp-livereload'),
-	plumber = require('gulp-plumber'),
-	gutil = require('gulp-util');
+    stylus = require('gulp-stylus'),
+    jade = require('gulp-jade'),
+    sourcemaps = require('gulp-sourcemaps'),
+    livereload = require('gulp-livereload'),
+    plumber = require('gulp-plumber'),
+    gutil = require('gulp-util'),
+    concat = require('gulp-concat'),
+    //babel = require('gulp-babel'),
+    source = require('vinyl-source-stream'),
+    buffer = require('vinyl-buffer'),
+    babelify = require('babelify'),
+    watchify = require('watchify'),
+    uglify = require('gulp-uglify'),
+    browserify = require('browserify');
 
 var watching = false;
 
@@ -20,19 +28,39 @@ function exitIfNotWatching(error) {
 }
 
 gulp.task('styles', function() {
-	gulp.src('./app/styl/app.styl')
-		.pipe(plumber(exitIfNotWatching))
-		.pipe(sourcemaps.init())
-		.pipe(stylus({
-			compress: true
-		}))
-		.pipe(sourcemaps.write('.'))
-		.pipe(gulp.dest('./out/static/'))
-		.pipe(livereload());
+    return gulp.src('./app/styl/app.styl')
+        .pipe(plumber(exitIfNotWatching))
+        .pipe(sourcemaps.init())
+        .pipe(stylus({
+            compress: true
+        }))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('./out/static/'))
+        .pipe(livereload());
+});
+
+gulp.task('scripts', function() {
+    var browserifyOpts = {
+        entries: './app.js',
+        basedir: './app/scripts/',
+        debug: true
+    };
+
+    return browserify(browserifyOpts)
+        .transform(babelify)
+        .bundle()
+        .pipe(plumber(exitIfNotWatching))
+        .pipe(source('app.js'))
+        .pipe(buffer())
+        .pipe(sourcemaps.init({loadMaps: true}))
+        .pipe(uglify())
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('./out/static/'))
+        .pipe(livereload());
 });
 
 gulp.task('templates', function() {
-	gulp.src('./app/templates/*.jade')
+	return gulp.src('./app/templates/*.jade')
 		.pipe(plumber(exitIfNotWatching))
 		.pipe(jade())
 		.pipe(gulp.dest('./out/'))
@@ -45,6 +73,7 @@ gulp.task('watch', function() {
 	livereload.listen();
 	gulp.watch('./app/styl/*', ['styles']);
 	gulp.watch('./app/templates/*', ['templates']);
+	gulp.watch('./app/scripts/*', ['scripts']);
 });
 
-gulp.task('default', ['styles', 'templates']);
+gulp.task('default', ['styles', 'templates', 'scripts']);
